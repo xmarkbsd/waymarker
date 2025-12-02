e84e0aade32ea1d1b67369b245fb04ab751b5cd7
Mark Salad <mandsal@gmail.com>
2025-12-02 16:52:14 +0000

    feat(attribution): move map attribution to Settings page for clean mobile UI

--- src/pages/MapView.tsx
+++ src/pages/MapView.tsx
@@ -47,6 +47,7 @@ export const MapView: React.FC<MapViewProps> = ({ onPlaceObservation, moveMode, 
         center={[51.505, -0.09]}
         zoom={13}
         style={{ height: '100%', width: '100%' }}
+        attributionControl={false}
       >
         <CustomOfflineTileLayer />
         <LayerControl />
